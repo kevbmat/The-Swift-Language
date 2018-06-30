a26ce7461ed074c78b33966f75a776887dd53f93
Kevin Mattappally <kevinmat@outlook.com>
2018-06-30 15:54:35 -0700

    Subscripts

--- TheSwiftProgrammingLanguage.playground/Pages/Subscripts.xcplaygroundpage/Contents.swift
+++ TheSwiftProgrammingLanguage.playground/Pages/Subscripts.xcplaygroundpage/Contents.swift
@@ -3,4 +3,25 @@
 
 import Foundation
 
-// METHODS (functions that associate with a particular type)
+// SUBSCRIPTS
+
+// subscript syntax
+/*
+subscript(index: Int) -> Int {
+    get {
+        // return an appropriate subscript value here
+    }
+    set(newValue) {
+        // perform a suitable setting action here
+    }
+}*/
+
+// example
+struct AddTogether {
+    let number: Int
+    subscript(index: Int) -> Int {
+        return index + number
+    }
+}
+let addSeven = AddTogether(number: 3)
+addSeven[7]
